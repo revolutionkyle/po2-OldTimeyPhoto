dbf78b0550e20a79af66a8a44130be57d9a9dcd6
revolutionkyle <khammer4@mail.csuchico.edu>
2017-10-04 16:35:07 -0700

    This version tells you the size of the image!

--- main.cpp
+++ main.cpp
@@ -1,7 +1,6 @@
 // Author: Kyle Hammer
 #include<fstream>
-#include<"bitmap.h"
-#include<cstdlib>
+#include"bitmap.h"
 #include<iostream>
 #include<string>
 #include<vector>
@@ -14,18 +13,28 @@ int main()
     Bitmap image;
     vector <vector <Pixel> > bmp;
     Pixel rgb;
-
+    bool isImage();
     // getting users input for a picture
-    do while(reset=='Y'||reset=='y')
-    cout << "please include an image file." << endl;
-    cin >> pictureLocation;
-    image.open(pictureLocation);
-    bmp = image.toPixelMatrix();
+    reset = 'Y';
+    while(reset =='Y' || reset == 'y')
+    {
+        cout << "Please enter a picture location." << endl;
+        cin >> pictureLocation;
 
+        image.open(pictureLocation);
+    
 
    // take users picture and translate it into a form that a computer can manipulate
+        if(image.isImage()==1)
+        { bmp = image.toPixelMatrix();
+      
+        cout << pictureLocation << " has been loaded. It is " << bmp[0].size() << "pixels wide and " << bmp.size() << "pixels high" << endl;
 
-
+        }
+        if(image.isImage()==0)
+        {cout << "That is not an image! Please insert an image" << endl;
+        reset =='Y';
+        }
    // (extra) ask the user what particular type of color manipulation they want done
 
 
@@ -35,6 +44,9 @@ int main()
    // save the new manipulated image as a picture file
 
 
-  // ask the user if they want to input another picture to convert. 
+  // ask the user if they want to input another picture to convert.
+  cout << "Do you want to insert another image? Y/N " << endl;
+  cin >> reset;
+  }
   return 0;
 }
