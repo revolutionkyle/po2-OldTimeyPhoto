fe5549a54ed38c23ba66da354d3cb90066735963
revolutionkyle <khammer4@mail.csuchico.edu>
2017-10-10 20:09:38 -0700

    Close to finished the main purpose, still wont save image but compiles.

--- main.cpp
+++ main.cpp
@@ -5,6 +5,7 @@
 #include<string>
 #include<vector>
 using namespace std;
+const int NUMC = 3; // Number of available colors for each of the pixels. Can be changed if you somehow get one of those crazy 4 color TV's
 int main()
 {  // variable declaration for the program
     
@@ -14,6 +15,7 @@ int main()
     vector <vector <Pixel> > bmp;
     Pixel rgb;
     bool isImage();
+    int average;
     // getting users input for a picture
     reset = 'Y';
     while(reset =='Y' || reset == 'y')
@@ -26,23 +28,29 @@ int main()
 
    // take users picture and translate it into a form that a computer can manipulate
         if(image.isImage()==1)
-        { bmp = image.toPixelMatrix();
-      
-        cout << pictureLocation << " has been loaded. It is " << bmp[0].size() << "pixels wide and " << bmp.size() << "pixels high" << endl;
+        {   bmp = image.toPixelMatrix();
+            // Find the average color and then set each color value to the average
+            for(int i = 0;i<bmp.size();i++)
+            {   for(int j =0;j<bmp[0].size();j++)
+                {
+                    rgb = bmp[i][j];
+                    average = rgb.red + rgb.blue + rgb.green % NUMC; 
+                    rgb.red = average;
+                    rgb.blue = average;
+                    rgb.green = average;
+                    bmp[i][j] = rgb;
+                 }
+             }
+         }
+         // Convert the matrix back into an image file and save the file
+         image.fromPixelMatrix(bmp);
+         image.save("OldTimey.bmp");
 
-        }
+     
         if(image.isImage()==0)
         {cout << "That is not an image! Please insert an image" << endl;
         reset =='Y';
         }
-   // (extra) ask the user what particular type of color manipulation they want done
-
-
-   // manipulate each pixel in the picture using a recursive process
-
-
-   // save the new manipulated image as a picture file
-
 
   // ask the user if they want to input another picture to convert.
   cout << "Do you want to insert another image? Y/N " << endl;
