81c4ed2e17f59108ba7ec4eb68949ab6c94570ed
revolutionkyle <khammer4@mail.csuchico.edu>
2017-10-13 09:59:37 -0700

    Close to the final version with the general usability.

--- main.cpp
+++ main.cpp
@@ -1,60 +1,54 @@
-// Author: Kyle Hammer
-#include<fstream>
 #include"bitmap.h"
-#include<iostream>
-#include<string>
 #include<vector>
+#include<string>
 using namespace std;
-const int NUMC = 3; // Number of available colors for each of the pixels. Can be changed if you somehow get one of those crazy 4 color TV's
 int main()
-{  // variable declaration for the program
-    
-    string pictureLocation;
-    char reset;
-    Bitmap image;
-    vector <vector <Pixel> > bmp;
-    Pixel rgb;
-    bool isImage();
-    int average;
-    // getting users input for a picture
-    reset = 'Y';
-    while(reset =='Y' || reset == 'y')
-    {
-        cout << "Please enter a picture location." << endl;
-        cin >> pictureLocation;
+{
+ Bitmap image;
+ vector <vector <Pixel> > bmp;
+ Pixel rgb;
+ int average;
+ string pictureLocation;
+ char reset;
+ string newName;
 
-        image.open(pictureLocation);
-    
+ do{ cout<< "Please insert the location of the file you would like to convert." << endl;
+     cin>> pictureLocation;
+ 
+     image.open(pictureLocation);
 
-   // take users picture and translate it into a form that a computer can manipulate
-        if(image.isImage()==1)
-        {   bmp = image.toPixelMatrix();
-            // Find the average color and then set each color value to the average
-            for(int i = 0;i<bmp.size();i++)
-            {   for(int j =0;j<bmp[0].size();j++)
-                {
-                    rgb = bmp[i][j];
-                    average = rgb.red + rgb.blue + rgb.green % NUMC; 
-                    rgb.red = average;
-                    rgb.blue = average;
-                    rgb.green = average;
-                    bmp[i][j] = rgb;
-                 }
-             }
-         }
-         // Convert the matrix back into an image file and save the file
-         image.fromPixelMatrix(bmp);
-         image.save("OldTimey.bmp");
+     bool validBmp = image.isImage();
 
-     
-        if(image.isImage()==0)
-        {cout << "That is not an image! Please insert an image" << endl;
-        reset =='Y';
+     if(validBmp == true)
+     {
+        bmp = image.toPixelMatrix();
+        for(int i =0; i < bmp.size(); i++)
+        {   for(int j = 0; j<bmp[0].size(); j++)
+            {   rgb = bmp[i][j];
+                average = (rgb.red + rgb.blue + rgb.green)/3 ;
+                rgb.red = average;
+                rgb.blue = average;
+                rgb.green = average;
+                bmp[i][j] = rgb;
+            }
         }
-
-  // ask the user if they want to input another picture to convert.
-  cout << "Do you want to insert another image? Y/N " << endl;
-  cin >> reset;
-  }
-  return 0;
-}
+        image.fromPixelMatrix(bmp);
+        cout << "What would you like to name your new image file? If you leave it blank it will be saved as OldTimey.bmp." << endl;
+        cin >> newName;
+        if(newName.size()==0)
+        {image.save("OldTimey.bmp");
+        }
+        else
+        {image.save(newName);
+        }
+    }
+    else if(validBmp == false)
+    { cout << "That isn't a picture! Please insert a new picture." << endl;
+      reset = 'Y';
+    }
+    cout << "Would you like to insert another picture? Y/N" << endl;
+    cin>> reset;
+   } while(reset == 'Y'|| reset == 'y');
+ return 0;
+ }
+ 
