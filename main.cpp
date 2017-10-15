0d0d7eb32252775ee7d45d447ae16f9b95add184
revolutionkyle <khammer4@mail.csuchico.edu>
2017-10-15 14:17:52 -0700

    Final Version with a few extra user interface improvements.

--- main.cpp
+++ main.cpp
@@ -1,9 +1,11 @@
 #include"bitmap.h"
 #include<vector>
 #include<string>
+#include<cmath>
 using namespace std;
+int const NUMCOLORS = 3;
 int main()
-{
+{ //Variables that will be used to convert the image.
  Bitmap image;
  vector <vector <Pixel> > bmp;
  Pixel rgb;
@@ -12,42 +14,43 @@ int main()
  char reset;
  string newName;
 
- do{ cout<< "Please insert the location of the file you would like to convert." << endl;
+ do{ // Get users input for an image to convert.
+     cout<< "Please insert the location of the file you would like to convert." << endl;
      cin>> pictureLocation;
  
      image.open(pictureLocation);
-
+     //Test to see if what the user input is an image.
      bool validBmp = image.isImage();
 
      if(validBmp == true)
-     {
+     {  //Covert image into a a matrix and begin looping through matrix.
         bmp = image.toPixelMatrix();
         for(int i =0; i < bmp.size(); i++)
         {   for(int j = 0; j<bmp[0].size(); j++)
-            {   rgb = bmp[i][j];
-                average = (rgb.red + rgb.blue + rgb.green)/3 ;
+            {   //Convert each pixel to black and white by averaging the color at each pixel.
+                rgb = bmp[i][j];
+                average = (rgb.red+rgb.blue+rgb.green)/NUMCOLORS; 
                 rgb.red = average;
                 rgb.blue = average;
                 rgb.green = average;
                 bmp[i][j] = rgb;
             }
         }
+        //Covert matrix back into an image.
         image.fromPixelMatrix(bmp);
-        cout << "What would you like to name your new image file? If you leave it blank it will be saved as OldTimey.bmp." << endl;
+        //Ask the user for what they would like to name the file. (Extra)
+        cout << "What would you like to name your new image file? Don't forget to put .bmp at the end of the file name!" << endl;
         cin >> newName;
-        if(newName.size()==0)
-        {image.save("OldTimey.bmp");
-        }
-        else
-        {image.save(newName);
-        }
+        image.save(newName);
+        //Ask the user if they want to input another image for converting.
+        cout << "Would you like to insert another picture for converting into black and white? Y/N" << endl;
+        cin >> reset;
     }
+    //In case the user does not input a picture. (so the program doesn't crash)
     else if(validBmp == false)
     { cout << "That isn't a picture! Please insert a new picture." << endl;
       reset = 'Y';
     }
-    cout << "Would you like to insert another picture? Y/N" << endl;
-    cin>> reset;
    } while(reset == 'Y'|| reset == 'y');
  return 0;
  }
